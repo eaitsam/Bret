3d7c99f005d210d905668ef3dbdddccf6f6262b4
bhendri1 <59148080+bhendri1@users.noreply.github.com>
2021-05-16 17:57:47 -0400

    Added Hover Text
    Added hover text statements for EBAY Electronics and Iphones.

--- facebook1/FacebookAutomation.cs
+++ facebook1/FacebookAutomation.cs
@@ -2,6 +2,7 @@ using Xunit;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
 
 namespace facebook1
 {
@@ -13,6 +14,18 @@ namespace facebook1
             IWebDriver driver = new ChromeDriver(); //Chrome thing is fixed
             
              driver.Url = "https://www.ebay.com/";
+            IWebElement Electronics = driver.FindElement(By.LinkText("Electronics"));
+            IWebElement Iphone = driver.FindElement(By.LinkText("iPhones"));
+           
+            Actions action = new Actions(driver);
+            action.MoveToElement(Electronics).Build().Perform();
+            action.MoveToElement(Iphone).Click();
+
+
+
+
+            
+
 
 
             /*IWebElement EmailAdd = driver.FindElement(By.Id("email"));
