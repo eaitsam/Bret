7d5e92517c6ef614b3343725e48d528b0b3bf2cf
bhendri1 <59148080+bhendri1@users.noreply.github.com>
2021-05-16 17:21:31 -0400

    Web Address changed
    Google was changed to EBAY and Firefox Driver was deleted.

--- facebook1/FacebookAutomation.cs
+++ facebook1/FacebookAutomation.cs
@@ -11,8 +11,9 @@ namespace facebook1
         public void Test1()
         {
             IWebDriver driver = new ChromeDriver(); //Chrome thing is fixed
-            IWebDriver driver = new FirefoxDriver();
-             driver.Url = "https://www.google.com/";
+            
+             driver.Url = "https://www.ebay.com/";
+
 
             /*IWebElement EmailAdd = driver.FindElement(By.Id("email"));
             IWebElement PasswordAdd = driver.FindElement(By.Id("pass"));
